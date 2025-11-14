a2b53ada5429fab57affe7c8aa4c4d0916828e4a
sanueee <109550742+sanueee@users.noreply.github.com>
2025-11-14 20:03:59 +0300

    added requesting file

--- 63.py
+++ 63.py
@@ -1,5 +1,13 @@
 import csv
+import requests
 from collections import Counter
+
+url = 'https://dfedorov.spb.ru/python3/sport.txt'
+response = requests.get(url)
+response.encoding = 'cp1251'
+
+with open('sport.txt', 'w', encoding='cp1251') as f:
+    f.write(response.text)
 
 sports = Counter()
 
@@ -12,6 +20,5 @@ with open('sport.txt', 'r', encoding='cp1251') as f:
             good = elem.lower().strip()
             if good:
                 sports[good] += 1
-
 
 print(sports.most_common(3))
